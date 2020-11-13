a804aebaeeb65efb0a62d69db0b41eaaf8b85c2c
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-11-13 16:13:24 +0700

    1.3 cleared

--- 1-3.clj
+++ 1-3.clj
@@ -7,30 +7,12 @@
           data))
 
 (defn my-filter [f data]
-  (reduce (fn [a b]
-              (if (f b)
-                  (conj a b)
-                  a))
-          []
-          data))
-
-(defn my-filter2 [f data]
   (reduce #(if (f %2)
                (conj %1 %2)
                %1)
           []
           data))
 
-(defn my-filter-nw [f data]
-  (reduce #((if (f %2)
-               (conj %1 %2)
-               %1))
-          []
-          data))
-
-#((if (f %2) (conj %1 %2) %1))
-
 (println (my-map #(* %1 %1) '(2 4 3)))
-(println (my-filter2 even? (range 1 10)))
-(println (my-filter-nw even? (range 1 10)))
+(println (my-filter even? (range 1 10)))
 
