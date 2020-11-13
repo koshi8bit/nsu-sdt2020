af7fedbe12970611fd51ed36f037c48a845d20ed
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-11-13 20:32:50 +0700

    2.2 ....

--- 2-2.clj
+++ 2-2.clj
@@ -3,7 +3,11 @@
 
 
 (defn calc-trap [f a b]
-  (* (/ (+ (f a) (f b)) 2) (- b a)))
+    (let [res (* (/ (+ (f a) (f b)) 2) (- b a))]
+        (println "calc-trap" res)
+        res
+    )
+  )
 
 ;;--------------
 ;; better way
@@ -21,18 +25,21 @@
 ;;--------------
 
 (defn get-integrator [f begin step]
-    (let [ls (iterate #(+ % (calc-trap f % (+ % step)))
-                      begin)]
+    (let [ls (iterate
+                #(+ % (calc-trap f % (+ % step)))
+                begin)]
         (fn [k]
+;;            (println "index" (/ k step))
+;;            (println (take 100 ls))
             (nth ls (/ k step))
         )
     )
 )
 
 (let [integrator (get-integrator (fn [x] x) 0 1)]
-    (time (integrator 10000))  ;; Elapsed time: 332.0941 msecs
-    (time (integrator 10015))  ;; Elapsed time: 1.2308 msecs
-    (time (integrator  9985))  ;; Elapsed time: 0.5012 msecs
+    ;(time (integrator 10000))  ;; Elapsed time: 332.0941 msecs
+    ;(time (integrator 10015))  ;; Elapsed time: 1.2308 msecs
+    (time (integrator     20))  ;; Elapsed time: 0.5012 msecs
     (time (integrator     1))  ;; 1/2 для y=x
     ;(time (integrator     2))  ;; 3/2 для y=x ???
 )
