7c4588f851c5d4612d11c15ef9db90d9c249e81f
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-11-13 20:30:10 +0700

    2.2 ...

--- 2-2.clj
+++ 2-2.clj
@@ -29,11 +29,11 @@
     )
 )
 
-(let [integrator (get-integrator (fn [x] x) 0 1/2)]
-    ;(time (integrator 10000))  ;; Elapsed time: 332.0941 msecs
-    ;(time (integrator 10015))  ;; Elapsed time: 1.2308 msecs
-    ;(time (integrator  9985))  ;; Elapsed time: 0.5012 msecs
+(let [integrator (get-integrator (fn [x] x) 0 1)]
+    (time (integrator 10000))  ;; Elapsed time: 332.0941 msecs
+    (time (integrator 10015))  ;; Elapsed time: 1.2308 msecs
+    (time (integrator  9985))  ;; Elapsed time: 0.5012 msecs
     (time (integrator     1))  ;; 1/2 для y=x
-    (time (integrator     2))  ;; 3/2 для y=x ???
+    ;(time (integrator     2))  ;; 3/2 для y=x ???
 )
 
