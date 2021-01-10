7f570a2abee0a75c68b24f58c0488129dcc64e00
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2021-01-11 01:18:28 +0700

    dnf 010203 ok

--- 4-1.clj
+++ 4-1.clj
@@ -85,19 +85,19 @@
 
 ;; NOT
 
-(defn f-inv [expr]
+(defn f-not [expr]
     (cons ::not expr)
 )
 
-(defn f-inv? [expr]
+(defn f-not? [expr]
     (= (first expr) ::not)
 )
 
-(defn f-inv-calc [expr var value]
+(defn f-not-calc [expr var value]
     (let [res (calculate (rest expr) var value)]
         (if (constant? res)
             (constant (not (constant-value res)))
-            (f-inv res)
+            (f-not res)
         )
     )
 )
@@ -150,12 +150,12 @@
 
 ;; parser
 
-(def calculate_rules
+(def calculate-rules
     (list
         [(fn [expr _ _] (constant? expr)) (fn [expr _ _] expr)]
         [(fn [expr v _] (same-variables? expr v)) (fn [_ _ value] value)]
         [(fn [expr _ _] (variable? expr)) (fn [expr _ _] expr)]
-        [(fn [expr _ _] (f-inv? expr)) f-inv-calc]
+        [(fn [expr _ _] (f-not? expr)) f-not-calc]
         [(fn [expr _ _] (f-or? expr)) f-or-calc]
         [(fn [expr _ _] (f-and? expr)) f-and-calc]
         [(fn [expr _ _] (f-impl? expr)) f-impl-calc]
@@ -164,14 +164,16 @@
 
 (defn calculate [expr var value]
     ;;(println expr var value)
-    ((some
+    (
+        (some
          (fn [rule]
              (if ((first rule) expr var value)
                  (second rule)
                  false))
-         calculate_rules
+         calculate-rules
+        )
+        expr var value
     )
-    expr var value)
 )
 
 (defn get-result [expr]
@@ -186,7 +188,7 @@
 (println "1" (get-result (calculate
     (f-xor
         (variable :a)
-        (f-inv (f-inv (variable :b)))
+        (f-not (f-not (variable :b)))
     )
     (variable :a) (constant true)))
 )
@@ -210,3 +212,76 @@
 ;;1 (:labs.core/xor (:labs.core/const true) (:labs.core/not :labs.core/not :labs.core/var :b))
 ;;2 true
 ;;3 (:labs.core/and ((:labs.core/const true) (:labs.core/var :b)))
+
+;; DNF
+;; https://ru.wikipedia.org/wiki/%D0%94%D0%B8%D0%B7%D1%8A%D1%8E%D0%BD%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%B0%D1%8F_%D0%BD%D0%BE%D1%80%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D1%84%D0%BE%D1%80%D0%BC%D0%B0
+
+(declare simplification)
+(def dnf01-rules
+    (list
+        [(fn [expr] (constant? expr)) (fn [expr] expr)]
+        [(fn [expr] (variable? expr)) (fn [expr] expr)]
+        [(fn [expr] (f-not? expr)) (fn [expr] (f-not (simplification (rest expr))))]
+        [(fn [expr] (f-or? expr)) (fn [expr] (apply f-or (map #(simplification %) (rest expr))))]
+        [(fn [expr] (f-and? expr)) (fn [expr] (apply f-and (map #(simplification %) (rest expr))))]
+        [(fn [expr] (f-impl? expr)) (fn [expr] (f-or (f-not (simplification (nth expr 1))) (simplification (nth expr 2))))]
+        [(fn [expr] (f-xor? expr)) (fn [expr] (f-or
+                                                    (f-and
+                                                        (f-not (simplification (nth expr 1)))
+                                                        (simplification (nth expr 2))
+                                                    )
+                                                    (f-and
+                                                        (simplification (nth expr 1))
+                                                        (f-not (simplification (nth expr 2)))
+                                                    )))]
+    )
+)
+
+(defn dnf01 [expr]
+    (
+        (some
+            (fn [rule]
+               (if ((first rule) expr)
+                 (second rule)
+                 false)
+            )
+            dnf01-rules
+        )
+        expr
+    )
+)
+
+(println "dnf01"
+    (get-result
+        (dnf01
+            (f-xor (variable :x) (variable :y))
+        )
+    )
+)
+
+(defn dnf0203 [expr]
+    (if (f-not? expr)
+        (let [expr2 (rest expr)]
+            (cond
+                (f-not? expr2) (dnf0203 (rest expr2))
+                (f-or? expr2) (apply f-and (map #(dnf0203 (f-not %)) (rest expr2)))
+                (f-and? expr2) (apply f-or (map #(dnf0203 (f-not %)) (rest expr2)))
+                :else (f-not expr2)
+            )
+        )
+        (if (variable? expr)
+            expr
+            (cons (first expr) (map #(dnf0203 %) (rest expr)))
+        )
+    )
+)
+
+(println "dnf0203"
+    (get-result
+        (dnf0203
+            (f-not (f-or (variable :x) (variable :y)))
+        )
+    )
+)
+
+
